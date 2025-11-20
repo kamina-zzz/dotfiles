a43b774eaadcde12cb882ef854b8d45c6a68f8b7
kamina-zzz <kamina.dev1991@gmail.com>
2025-11-20 10:53:26 +0900

    fix wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,74 +5,27 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
-config.automatically_reload_config = true
-config.font_size = 12.0
-config.use_ime = true
-
 config.color_scheme = 'iceberg-dark'
 
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 20
 
 ----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- タイトルバーを非表示
-config.window_decorations = "RESIZE"
--- タブバーの表示
+-- show tab bar
 config.show_tabs_in_tab_bar = true
--- タブが一つの時は非表示
-config.hide_tab_bar_if_only_one_tab = true
--- falseにするとタブバーの透過が効かなくなる
-config.use_fancy_tab_bar = false
-
--- タブ同士の境界線を非表示
-config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
-}
-
--- タブの形をカスタマイズ
--- タブの左側の装飾
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
--- タブの右側の装飾
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
-  local edge_background = "none"
-  if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
-  end
-  local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
-end)
 
 ----------------------------------------------------
 -- keybinds
 ----------------------------------------------------
 local act = wezterm.action
-config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "S", mods = "CTRL|SHIFT", timeout_milliseconds = 2000 }
 config.keys = {
   { key = "p", mods = "SUPER", action = act.ActivateCommandPalette },
   { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+  { key = 'Enter', mods = 'ALT', action = act.DisableDefaultAssignment },
 }
 
 -- Finally, return the configuration to wezterm:
